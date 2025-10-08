fd6b2d960631076ec2731777d27ae2849bf959a3
MónicaMorales <77685820+HTK-MM@users.noreply.github.com>
2025-10-08 16:12:47 +0200

    Fix CORS policy for frontend domain

--- CoderrBackend/settings.py
+++ CoderrBackend/settings.py
@@ -33,11 +33,7 @@ ALLOWED_HOSTS = []
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    'http://coderr.monica-morales.com',
-    'https://coderr.monica-morales.com',
-    
-]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -56,13 +52,18 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',   
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',    
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://coderr.monica-morales.com',
+    'https://coderr.monica-morales.com',    
 ]
 
 ROOT_URLCONF = 'CoderrBackend.urls'
